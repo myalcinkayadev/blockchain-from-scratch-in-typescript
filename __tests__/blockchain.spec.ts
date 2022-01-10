e00d81413b1b990c4121b2cdaf1bcbbeb8905c5a
Mehmet Yalcinkaya <mehmetyalcinkaya042@gmail.com>
2022-01-10 23:07:32 +0300

    Add valid and corrupt chain tests to blockchain

--- __tests__/blockchain.spec.ts
+++ __tests__/blockchain.spec.ts
@@ -2,10 +2,11 @@ import { Blockchain } from '../src/blockchain';
 import { Block } from '../src/block';
 
 describe('blockchain', () => {
-  let blockchain: Blockchain;
+  let blockchain: Blockchain, blockchain2: Blockchain;
 
   beforeEach(() => {
     blockchain = new Blockchain();
+    blockchain2 = new Blockchain();
   });
 
   test('it starts with genesis block', () => {
@@ -17,5 +18,24 @@ describe('blockchain', () => {
     blockchain.addBlock(data);
 
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+  });
+
+  test('it validates a valid chain', () => {
+    blockchain2.addBlock('foo');
+
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(true);
+  });
+
+  test('it invalidates a chain with a corrupt genesis block', () => {
+    blockchain2.chain[0].data = 'bad data';
+
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+  });
+
+  test('it invalidates a corrupt chain', () => {
+    blockchain2.addBlock('foo');
+    blockchain2.chain[1].data = 'not foo';
+
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
   });
 });
