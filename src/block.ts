f4d28245a3a7c131e4c7ba697c4a3ae8cbacdda8
Mehmet Yalcinkaya <mehmetyalcinkaya042@gmail.com>
2022-01-10 16:20:52 +0300

    Add hash function to block

--- src/block.ts
+++ src/block.ts
@@ -1,3 +1,10 @@
+import { createHmac } from 'crypto';
+import { BLOCK_HASH_PRIVATE_KEY } from './config';
+
+function assertBlockHashPrivateKey(value: unknown): asserts value is string {
+  if (typeof value !== 'string') throw new TypeError('block hash private key is must be a string');
+}
+
 class Block {
   constructor(
     public timestamp: number,
@@ -21,9 +28,15 @@ class Block {
   static mineBlock(lastBlock: Block, data: unknown) {
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
-    const hash = 'todo-hash';
+    const hash = Block.hash(timestamp, lastHash, data);
 
     return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp: number, lastHash: string, data: unknown) {
+    assertBlockHashPrivateKey(BLOCK_HASH_PRIVATE_KEY);
+    const sha256Hasher = createHmac('sha256', BLOCK_HASH_PRIVATE_KEY);
+    return sha256Hasher.update(`${timestamp}${lastHash}${data}`).digest('hex');
   }
 }
 
