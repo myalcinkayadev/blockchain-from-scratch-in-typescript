dbcbab0bcec041f50bbeee6d5d0faccfc14ad0d4
Mehmet Yalcinkaya <mehmetyalcinkaya042@gmail.com>
2022-01-12 19:10:55 +0300

    Add dynamic block difficulty tests to block

--- __tests__/blockchain/block.spec.ts
+++ __tests__/blockchain/block.spec.ts
@@ -16,4 +16,16 @@ describe('block', () => {
   test('it sets the `lastHash` to match the hash of the last block', () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
   });
+
+  test('it generates a hash that matches the difficulty', () => {
+    expect(block.hash.substring(0, block.difficulty)).toBe('0'.repeat(block.difficulty));
+  });
+
+  test('it lowers the difficulty for slowly mined blocks', () => {
+    expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(block.difficulty - 1);
+  });
+
+  test('it raises the difficulty for quickly mined blocks', () => {
+    expect(Block.adjustDifficulty(block, block.timestamp + 1)).toEqual(block.difficulty + 1);
+  });
 });
