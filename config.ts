b8af5aaf41fe6d61ee615dd763206f1a0ddb5536
Mehmet Yalcinkaya <mehmetyalcinkaya042@gmail.com>
2022-01-12 17:19:27 +0300

    Add block difficulty and mine rate to config

--- config.ts
+++ config.ts
@@ -3,10 +3,21 @@ import assert from 'assert';
 
 dotenv.config();
 
-const { HTTP_PORT, P2P_PORT, BLOCK_HASH_PRIVATE_KEY } = process.env;
+const { HTTP_PORT, P2P_PORT, BLOCK_HASH_PRIVATE_KEY, GENESIS_BLOCK_HEADERS_DIFFICULTY } =
+  process.env;
 
 assert(HTTP_PORT, 'HTTP_PORT is required!');
 assert(P2P_PORT, 'P2P_PORT is required!');
 assert(BLOCK_HASH_PRIVATE_KEY, 'BLOCK_HASH_PRIVATE_KEY is required!');
 
-export { HTTP_PORT, P2P_PORT, BLOCK_HASH_PRIVATE_KEY };
+const GENESIS_DATA = {
+  blockHeaders: {
+    difficulty: Number.parseInt(GENESIS_BLOCK_HEADERS_DIFFICULTY ?? '1'),
+  },
+};
+
+const MILLISECONDS = 1;
+const SECONDS = 1000 * MILLISECONDS;
+const MINE_RATE = 3 * SECONDS;
+
+export { GENESIS_DATA, MINE_RATE, HTTP_PORT, P2P_PORT, BLOCK_HASH_PRIVATE_KEY };
