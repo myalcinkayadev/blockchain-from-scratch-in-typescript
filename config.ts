268546191b93371eee001c74acfdb0d60c77ec56
Mehmet Yalcinkaya <mehmetyalcinkaya042@gmail.com>
2022-01-12 11:23:39 +0300

    Add HTTP and P2P ports to config.ts

--- config.ts
+++ config.ts
@@ -3,8 +3,10 @@ import assert from 'assert';
 
 dotenv.config();
 
-const { BLOCK_HASH_PRIVATE_KEY } = process.env;
+const { HTTP_PORT, P2P_PORT, BLOCK_HASH_PRIVATE_KEY } = process.env;
 
+assert(HTTP_PORT, 'HTTP_PORT is required!');
+assert(P2P_PORT, 'P2P_PORT is required!');
 assert(BLOCK_HASH_PRIVATE_KEY, 'BLOCK_HASH_PRIVATE_KEY is required!');
 
-export { BLOCK_HASH_PRIVATE_KEY };
+export { HTTP_PORT, P2P_PORT, BLOCK_HASH_PRIVATE_KEY };
