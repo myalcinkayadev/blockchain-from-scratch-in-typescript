e005ce76032c37d17bdcd67e1d5f0c084e5ea40a
Mehmet Yalcinkaya <mehmetyalcinkaya042@gmail.com>
2022-01-12 11:30:44 +0300

    Add blocks and mine routes

--- app/server.ts
+++ app/server.ts
@@ -1,12 +1,25 @@
 import * as restify from 'restify';
 import morgan from 'morgan';
 import { HTTP_PORT } from '../config';
-import { Blockchain } from '../blockchain';
 import { log } from '../logger';
+
+import { Blockchain } from '../blockchain';
 
 const server = restify.createServer();
 const blockchain = new Blockchain();
 
 server.use(morgan('common'));
 server.use(restify.plugins.bodyParser());
+
+server.get('/blocks', (_req, res) => {
+  res.json(blockchain.chain);
+});
+
+server.post('/mine', (req, res, next) => {
+  const { data } = req.body;
+  blockchain.addBlock(data);
+  // Todo Sync Chains
+  res.redirect('/blocks', next);
+});
+
 server.listen(HTTP_PORT, () => log.info(`Blockchain API listening on ${HTTP_PORT}`));
